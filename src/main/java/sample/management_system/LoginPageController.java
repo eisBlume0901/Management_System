38918d2c83a5b845067d7b8a2449d94bf9b9e264
Mary Kate Anecito <marykate09@protonmail.com>
2024-02-02 19:57:46 +0800

    Added loginButton and cancelButton and its actionPerformed functions in LoginPageController.java

--- src/main/java/sample/management_system/LoginPageController.java
+++ src/main/java/sample/management_system/LoginPageController.java
@@ -1,4 +1,25 @@
 package sample.management_system;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class LoginPageController {
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    protected void login(ActionEvent actionEvent)
+    {
+        System.out.println("Successfully login");
+    }
+
+    @FXML
+    protected void cancel(ActionEvent actionEvent)
+    {
+        System.out.println("Cancel login");
+    }
 }
